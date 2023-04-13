e6b6c1c34711b1846b537f11cc251459387e71f8
JaredGaastra <JGaastra2@Gmail.com>
2023-04-12 21:34:50 -0700

    practice

---
+++ queries_playstore.sql
@@ -0,0 +1,24 @@
+-- Comments in SQL Start with dash-dash --
+--1.SELECT * FROM analytics WHERE id = 1880;
+--2. SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+--3. SELECT category, COUNT(id) FROM analytics GROUP BY (category);
+--4. SELECT app_name, reviews FROM analytics ORDER BY reviews DESC LIMIT 5;
+--5. SELECT app_name, reviews FROM analytics WHERE rating >= 4.8 ORDER BY reviews DESC LIMIT 1;
+--6. SELECT category, AVG(rating) FROM analytics GROUP BY category,rating  ORDER BY rating DESC;
+--7. SELECT app_name, rating, price FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+--8. SELECT app_name, rating FROM analytics WHERE min_installs < 50 AND rating IS NOT NULL ORDER BY rating DESC;
+--9.SELECT app_name FROM analytics WHERE rating < 3 AND reviews >= 10000;
+--10. SELECT app_name FROM analytics WHERE price > 0.10 AND price < 1.00 ORDER BY price DESC LIMIT 10;
+--11. SELECT app_name FROM analytics ORDER BY last_updated ASC LIMIT 1;
+--12.SELECT app_name FROM analytics WHERE price = (SELECT MAX(price) FROM analytics);
+--13. SELECT COUNT(reviews) FROM analytics;
+--14.SELECT category FROM analytics GROUP BY category HAVING count(*) > 300;
+--15. SELECT app_name, reviews, min_installs, 
+--        CAST(min_installs AS FLOAT)/reviews AS proportion 
+-- FROM analytics 
+-- WHERE min_installs >= 100000 AND reviews > 0 
+-- ORDER BY proportion DESC 
+-- LIMIT 1;
+
+
+--FS1. 
---
+++ queries_products.sql
@@ -0,0 +1,15 @@
+-- Comments in SQL Start with dash-dash --
+-- 1. INSERT INTO products(name, price, can_be_returned) VALUES ('chair', 44.00, False);
+-- 2. INSERT INTO products(name, price, can_be_returned) VALUES('stool', 25.99, True);
+-- 3. INSERT INTO products(name, price, can_be_returned) VALUES('table', 124.00, False);
+-- 4. SELECT * FROM products; 
+-- 5. SELECT name FROM products;
+-- 6. SELECT name, price FROM products;
+-- 7. INSERT INTO products(name, price, can_be_returned) VALUES ('couch', 500, True);
+--8. SELECT name FROM products WHERE can_be_returned = True;
+--9. SELECT name FROM products WHERE price < 44.00;
+--10. SELECT name FROM products WHERE price < 99.99 AND price > 22.50;
+--11.  UPDATE products SET price = price * 0.8;
+--12. DELETE FROM products WHERE price < 25.00;
+--13. UPDATE products SET price = price + 20.00;
+--14. UPDATE products SET can_be_returned = True;
